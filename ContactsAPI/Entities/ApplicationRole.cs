ddf11dc30ada72fd08eedbc3dc3d3e8925239e0e
RSAmaro <acamaro9@hotmail.com>
2022-03-30 17:24:04 +0100

    Authentication Reconstruction

---
+++ ContactsAPI/Entities/ApplicationRole.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.SmartEnum;
+using Microsoft.AspNetCore.Identity;
+
+namespace ContactsAPI.Entities
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole()
+        {
+
+        }
+
+        public ApplicationRole(string roleName) : base(roleName)
+        {
+
+        }
+
+        public ICollection<ApplicationUserRole> UserRoles { get; set; }
+
+    }
+
+    public sealed class Roles: SmartEnum<Roles, string>
+    {
+        public static readonly Roles Admin = new("Admin", "Admin");
+        public static readonly Roles Member = new("Member", "Member");
+
+        protected Roles(string name, string value): base(name, value) 
+        {
+
+        }
+    }
+}
